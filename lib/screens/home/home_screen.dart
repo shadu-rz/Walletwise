6a517448c6c7f502125291b3c49e8a1abf661f76
Mohamed Shaduli <mshadulykk76@gmail.com>
2023-04-05 19:52:40 +0530

    almost bug fixes

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -134,6 +134,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ],
                             );
                           },
+                          //
                         )
                       ],
                     ),
